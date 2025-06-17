d69031023af853788b199cd1a6d5216d4aacda82
MatousFiala <matousfiala@gmail.com>
2025-06-17 17:22:22 +0200

    fixed on build

--- IROP Report/visualisations.js
+++ IROP Report/visualisations.js
@@ -39,8 +39,8 @@ let updateIropPodporaVis = function() {
         console.log("a");
 
         Promise.all([
-            d3.csv("https://matousfiala.cz/IROP%20Report/Data/oblasti.csv"),
-            d3.json("https://matousfiala.cz/IROP%20Report/Data/kraje.geojson")
+            d3.csv("https://matousfiala.cz/IROP%20Report/data/oblasti.csv"),
+            d3.json("https://matousfiala.cz/IROP%20Report/data/kraje.geojson")
         ]).then(function ([csvData, topoData]) {
             // Join data based on kraj and nuts3_kod
             let data = csvData.map(d => ({
